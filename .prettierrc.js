26015c9bddb610523164d0ccf5dfb67f1c0c3118
Max Zinovik <zinovik@gmail.com>
2023-12-18 20:19:06 +0100

    add 18.1

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	arrowParens: 'always',
+};
